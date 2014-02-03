26d8fc3ac5fe95d206dc88fb619aee0ec1eaa83a
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2014-02-03 14:06:00 +0100

    Done

--- tennis.go
+++ tennis.go
@@ -1,21 +1,74 @@
 package tennis
 
-import (
-	"strconv"
-)
+import "fmt"
 
 type Game struct {
-	playerOneScore int
+	scorePlayerOne int
+	namePlayerOne  string
+	scorePlayerTwo int
+	namePlayerTwo  string
 }
 
 func (g *Game) Score() string {
-	return strconv.FormatInt(int64(g.playerOneScore), 10) + "-0"
+	if g.isDeuce() {
+		return "Deuce"
+	}
+	if ok, player := g.hasAdvantage(); ok {
+		return "Advantage " + player
+	}
+	if ok, player := g.hasWinner(); ok {
+		return player + " wins"
+	}
+
+	return translateScore(g.scorePlayerOne) + "-" + translateScore(g.scorePlayerTwo)
+}
+
+func (g *Game) isDeuce() bool {
+	return g.scorePlayerOne == g.scorePlayerTwo && g.scorePlayerOne >= 3
+}
+
+func (g *Game) hasAdvantage() (bool, string) {
+	if g.scorePlayerOne > 3 && g.scorePlayerOne == g.scorePlayerTwo+1 {
+		return true, g.namePlayerOne
+	}
+	if g.scorePlayerTwo > 3 && g.scorePlayerTwo == g.scorePlayerOne+1 {
+		return true, g.namePlayerTwo
+	}
+	return false, ""
+}
+
+func (g *Game) hasWinner() (bool, string) {
+	if g.scorePlayerOne > 3 && g.scorePlayerOne >= g.scorePlayerTwo+2 {
+		return true, g.namePlayerOne
+	}
+	if g.scorePlayerTwo > 3 && g.scorePlayerTwo >= g.scorePlayerOne+2 {
+		return true, g.namePlayerTwo
+	}
+	return false, ""
+}
+
+func translateScore(score int) string {
+	switch score {
+	case 0:
+		return "0"
+	case 1:
+		return "15"
+	case 2:
+		return "30"
+	case 3:
+		return "40"
+	}
+	return fmt.Sprintf("## Unhandled score %d ##", score)
 }
 
 func (g *Game) PlayerOneScores() {
-	g.playerOneScore++
+	g.scorePlayerOne++
 }
 
-func NewGame() *Game {
-	return &Game{}
+func (g *Game) PlayerTwoScores() {
+	g.scorePlayerTwo++
+}
+
+func NewGame(playerOne, playerTwo string) *Game {
+	return &Game{namePlayerOne: playerOne, namePlayerTwo: playerTwo}
 }
--- tennis_test.go
+++ tennis_test.go
@@ -2,22 +2,148 @@ package tennis
 
 import "testing"
 
-func TestNewGameReturnsEgalite(t *testing.T) {
-	game := NewGame()
+var game *Game
 
-	s:=game.Score()
-	if s != "0-0" {
-		t.Errorf("Expected score 0-0 but was %s\n", s)
+func setup() {
+	game = NewGame("Agassi", "Djoko")
+}
+
+func assertScore(t *testing.T, expected string) {
+	s := game.Score()
+	if s != expected {
+		t.Errorf("Expected score \"%s\" but was \"%s\"", expected, s)
 	}
 }
 
-func TestPlayerOneScores(t *testing.T) {
-	game := NewGame()
-
-	game.PlayerOneScores()
-	
-	s:=game.Score()
-	if s != "1-0" {
-		t.Errorf("Expected score 1-0 but was %s\n", s)
+func createScore(scorePlayerOne, scorePlayerTwo int) {
+	for i := 0; i < scorePlayerOne; i++ {
+		game.PlayerOneScores()
 	}
+	for i := 0; i < scorePlayerTwo; i++ {
+		game.PlayerTwoScores()
+	}
+}
+func TestNewGameReturnsEgalite(t *testing.T) {
+	setup()
+
+	assertScore(t, "0-0")
+}
+
+func TestPlayerOneScores(t *testing.T) {
+	setup()
+
+	createScore(1, 0)
+
+	assertScore(t, "15-0")
+}
+
+func TestPlayerOneScores2(t *testing.T) {
+	setup()
+
+	createScore(2, 0)
+
+	assertScore(t, "30-0")
+}
+
+func TestPlayerOneScores3(t *testing.T) {
+	setup()
+
+	createScore(3, 0)
+
+	assertScore(t, "40-0")
+}
+func TestPlayerOneWins(t *testing.T) {
+	setup()
+
+	createScore(4, 0)
+
+	assertScore(t, "Agassi wins")
+}
+
+func TestPlayerTwoScores(t *testing.T) {
+	setup()
+
+	createScore(0, 1)
+
+	assertScore(t, "0-15")
+}
+
+func TestPlayerTwoScores2(t *testing.T) {
+	setup()
+
+	createScore(0, 2)
+
+	assertScore(t, "0-30")
+}
+
+func TestPlayerTwoScores3(t *testing.T) {
+	setup()
+
+	createScore(0, 3)
+
+	assertScore(t, "0-40")
+}
+
+func TestPlayerTwoWins(t *testing.T) {
+	setup()
+
+	createScore(0, 4)
+
+	assertScore(t, "Djoko wins")
+}
+
+func TestFifteenAll(t *testing.T) {
+	setup()
+
+	createScore(1, 1)
+
+	assertScore(t, "15-15")
+}
+
+func TestDeuce(t *testing.T) {
+	setup()
+
+	createScore(3, 3)
+
+	assertScore(t, "Deuce")
+}
+
+func TestPlayerOneAdvantage(t *testing.T) {
+	setup()
+
+	createScore(4, 3)
+
+	assertScore(t, "Advantage Agassi")
+}
+
+func TestPlayerTwoAdvantage(t *testing.T) {
+	setup()
+
+	createScore(3, 4)
+
+	assertScore(t, "Advantage Djoko")
+}
+
+func TestDeuceAfterAdvantage(t *testing.T) {
+	setup()
+
+	createScore(4, 4)
+
+	assertScore(t, "Deuce")
+}
+
+func TestPlayerOneWinsAfterAdvantage(t *testing.T) {
+	setup()
+
+	createScore(6, 4)
+
+	assertScore(t, "Agassi wins")
+}
+
+func TestPlayerTwoWinsAfterAdvantage(t *testing.T) {
+	setup()
+
+	createScore(7, 9)
+
+	assertScore(t, "Djoko wins")
 }
